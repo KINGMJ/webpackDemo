2fbf47b5069f328ded9cab93ab45367c754fa767
JerryMei <328047478@qq.com>
2017-11-17 13:49:05 +0800

    demo done

--- README.md
+++ README.md
@@ -1,6 +1,36 @@
-# webpackDemo
-webpack脚手架示例
+# 【webpack快速上手】2. 多Entry配置多页面应用
 
-> 这个分支是webpack进阶的一些配置，包括一些常用插件的使用，loaders配置，开发环境配置等
+前面介绍通过配置 Entry 和 Output 可以指定一个入口 js 来管理项目的依赖，最终打包成 bundle.js 。 这种对于单页面应用是可行的，但对于多页面应用通常不同的页面需要引入不同的 js 。对于这种场景，我们就要配置多入口了。
 
-说明见：http://www.maplejoyous.cn/webpack-demo2/
+## 多入口配置
+
+![image_1bv477m3e5e913701jht1eb410139.png-6.7kB][1]
+
+可以看到，entry 是支持对象的写法的，我们指定 key 和 name 就可以了
+
+**`webpack.config.js`**
+```
+entry: {
+    page_one: './src/app/page_one.js',
+    page_two: './src/app/page_two.js',
+    page_three: './src/app/page_three.js'
+}
+```
+
+同样的 Output 也要做出一些改变，我们需要为每个入口打包成一个单独的 js ，所以他们得有一个唯一的名字
+
+**`webpack.config.js`**
+```
+output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist')
+}
+```
+
+这里的`[name]`指代的就是 entry 里的 key 的名字，最终打包的 js 如下：
+
+![image_1bv47uag6if61nkj1qbu1av128fm.png-8.2kB][2]
+
+
+  [1]: http://static.zybuluo.com/Jerry-MEI/9nf6ha103jbjsvgippxqgzfn/image_1bv477m3e5e913701jht1eb410139.png
+  [2]: http://static.zybuluo.com/Jerry-MEI/rytrku4ja0h1vy5q9iwcu60t/image_1bv47uag6if61nkj1qbu1av128fm.png
--- webpack.config.js
+++ webpack.config.js
@@ -1,47 +1,17 @@
 const path = require('path');
-const CopyWebpackPlugin = require('copy-webpack-plugin');   //copy文件到输出目录
-const HtmlWebpackPlugin = require('html-webpack-plugin');   //自动生成HTML文件
-const CleanWebpackPlugin = require('clean-webpack-plugin'); //清空文件
 
 const config = {
     //入口文件
     entry: {
-        app: './src/app/index.js',
-        print: './src/app/print.js'
+        page_one: './src/app/page_one.js',
+        page_two: './src/app/page_two.js',
+        page_three: './src/app/page_three.js'
     },
     //入口文件输出配置
     output: {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist')
-    },
-    //loaders配置
-    module: {
-        rules: [
-            {test: /\.css$/, use: ['style-loader', 'css-loader']},
-            {test: /\.(png|svg|jpg|gif)$/, use: ['file-loader']},
-            {test: /\.(woff|woff2|eot|ttf|otf)$/, use: ['file-loader']},
-            {test: /\.(csv|tsv)$/, use: ['csv-loader']},
-            {test: /\.xml$/, use: ['xml-loader']},
-            {
-                test: /\.js$/,
-                exclude: /(node_modules|bower_components)/,
-                use: {loader: 'babel-loader', options: {presets: ['env']}}
-            }
-        ]
-    },
-    plugins: [
-        new CleanWebpackPlugin(['dist']),
-        new HtmlWebpackPlugin({
-            template: __dirname + '/src/index.html',
-            inject: 'body',
-            hash: true,
-            minify: {
-                "removeAttributeQuotes": true,
-                "removeComments": true,
-                "removeEmptyAttributes": true
-            }
-        })
-    ]
+    }
 };
 
 module.exports = config;
